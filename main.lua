cf381629c2cabd4243fdfef7b0a5c1a06d1c40be
azenbest <melvyncalcoen1@gmail.com>
2025-02-07 18:05:02 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -69,7 +69,7 @@ request({
 
 
 local success, allowed = pcall(function()
-	return  loadstring(game:HttpGet("https://raw.githubusercontent.com/azenbest/muscle-legend-script/refs/heads/main/main.lua"))()
+	return  loadstring(game:HttpGet("https://raw.githubusercontent.com/azenbest/IIIIIIIIIIIIIIIIIIIIIIIIIIIIII/refs/heads/main/IlIIIIIIIllIllIlllIIl"))()
 end)
 
 if not success or not allowed then
